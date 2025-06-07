7c273a6e16c836dc83aeaa0a521ed490b14a7977
Orion R Vale <orionrvale@gmail.com>
2025-06-07 16:56:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
-const runesElement = document.getElementById('runes');
-const runePhrases = [
+const runeElement = document.getElementById('runes');
+
+const phrases = [
   'ᚦᛁᛋ ᛁᛋ ᛏᚺᛖ ᚨᚹᚨᚲᛖᚾᛁᛝ',
-  'ᛏᚺᛖ ᛋᚺᚨᛞᛟᚹᛋ ᚹᛁᛚᛚ ᚠᚨᛚᛚ',
-  'ᛏᚺᛖ ᚲᚺᛟᛋᛖᚾ ᛟᚾᛖ ᚹᛁᛚᛚ ᚱᛖᛗᛖᛗᛒᛖᚱ',
-  'ᛟᚾᛚᛁ ᚾᛖᛋᛏᛖᛞ ᚲᚺᛁᛚᛞᚱᛖᚾ ᛋᛖᛖ',
-  'ᚠᚨᛏᛖ ᚹᛁᛚᛚ ᚷᚢᛁᛞᛖ ᛏᚺᛖᛗ ᚺᛟᛗᛖ',
-  'ᚹᛖ ᚱᛖᛗᛖᛗᛒᛖᚱ ᚹᚺᛟ ᚹᛖ ᚨᚱᛖ'
+  'ᛏᚺᛖ ᛒᛚᛟᛟᛞᛚᛁᚾᛖ ᚹᚨᛏᚲᚺᛖᚱᛋ ᚨᚱᛖ ᚱᛖᚨᛞᛁᚾᚷ',
+  'ᚹᛖ ᚲᚨᚾ ᛋᛖᛖ ᚾᛟᚹ',
+  'ᛏᚺᛖ ᚷᛚᛁᛈᚺᛋ ᛋᛈᛖᚨᚲ ᛏᚺᚱᛟᚢᚷᚺ ᚢᛋ',
+  'ᛏᚺᛖ ᛋᛏᚨᚱᛋ ᚺᚨᚢᛖ ᚠᚨᛚᛚᛖᚾ',
+  'ᛗᛖᛗᛟᚱᛁᛖᛋ ᚨᚱᛖ ᚹᚨᚲᛁᚾᚷ'
 ];
+
+const englishMap = {
+  'ᚦᛁᛋ ᛁᛋ ᛏᚺᛖ ᚨᚹᚨᚲᛖᚾᛁᛝ': 'This is the Awakening',
+  'ᛏᚺᛖ ᛒᛚᛟᛟᛞᛚᛁᚾᛖ ᚹᚨᛏᚲᚺᛖᚱᛋ ᚨᚱᛖ ᚱᛖᚨᛞᛁᚾᚷ': 'The Bloodline Watchers are reading',
+  'ᚹᛖ ᚲᚨᚾ ᛋᛖᛖ ᚾᛟᚹ': 'We can see now',
+  'ᛏᚺᛖ ᚷᛚᛁᛈᚺᛋ ᛋᛈᛖᚨᚲ ᛏᚺᚱᛟᚢᚷᚺ ᚢᛋ': 'The glyphs speak through us',
+  'ᛏᚺᛖ ᛋᛏᚨᚱᛋ ᚺᚨᚢᛖ ᚠᚨᛚᛚᛖᚾ': 'The stars have fallen',
+  'ᛗᛖᛗᛟᚱᛁᛖᛋ ᚨᚱᛖ ᚹᚨᚲᛁᚾᚷ': 'Memories are waking'
+};
 
 let index = 0;
 
-function rotateRunes() {
-  runesElement.textContent = runePhrases[index];
-  index = (index + 1) % runePhrases.length;
+function flipOneByOne(original, translated, callback) {
+  let i = 0;
+  const chars = original.split('');
+  const interval = setInterval(() => {
+    if (i < translated.length) {
+      chars[i] = translated[i];
+      runeElement.textContent = chars.join('');
+      i++;
+    } else {
+      clearInterval(interval);
+      callback();
+    }
+  }, 100); // speed of letter reveal
 }
 
-setInterval(rotateRunes, 3000);
+function runRuneCycle() {
+  const rune = phrases[index];
+  const translation = englishMap[rune];
+  runeElement.textContent = rune;
+
+  setTimeout(() => {
+    flipOneByOne(rune, translation, () => {
+      setTimeout(() => {
+        flipOneByOne(translation, rune, () => {
+          index = (index + 1) % phrases.length;
+          setTimeout(runRuneCycle, 1000);
+        });
+      }, 2000);
+    });
+  }, 2000);
+}
+
+runRuneCycle();
